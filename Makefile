9a8b5cd56ad93131d0415d332e8d24c5bd1b22ec
atarashi <atarashi@ca317041-1a13-0410-b3bf-f3d8fb9af60d>
2007-08-14 02:37:48 +0000

    * Avoid prohibition of overriding src_postinst. * Remove unnecessary removal in prepremove script. * Add Makefile. * Adjust properties.
    git-svn-id: https://yak.myhome.cx/repos/source/cygport-usrlocal/trunk@418 ca317041-1a13-0410-b3bf-f3d8fb9af60d

---
+++ Makefile
@@ -0,0 +1,4 @@
+VER=0.1-1
+
+dist:
+	tar cvjf cygport-usrlocal-$(VER).tar.bz2 --exclude .svn etc usr
--- etc/preremove/cygport-usrlocal.sh
+++ etc/preremove/cygport-usrlocal.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 rm -f /usr/lib/cygport/bin/prep_gnu_info_usrlocal.sh
-rm -f /usr/lib/cygport/lib/usrlocal.cygclass
