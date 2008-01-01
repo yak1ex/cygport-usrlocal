a1a340b882b4390d4aa4507cd9066810c6e58eee
atarashi <atarashi@ca317041-1a13-0410-b3bf-f3d8fb9af60d>
2008-01-01 03:24:36 +0000

    * etc/postinstall/cygport-usrlocal.sh: Fix bindir is not adjusted in mydobin.
    git-svn-id: https://yak.myhome.cx/repos/source/cygport-usrlocal/trunk@535 ca317041-1a13-0410-b3bf-f3d8fb9af60d

--- etc/postinstall/cygport-usrlocal.sh
+++ etc/postinstall/cygport-usrlocal.sh
@@ -2,5 +2,5 @@
 
 sed -e 's,/usr/share/info,/usr/local/info,g' /usr/lib/cygport/bin/prep_gnu_info.sh > /usr/lib/cygport/bin/prep_gnu_info_usrlocal.sh
 chmod +x /usr/lib/cygport/bin/prep_gnu_info_usrlocal.sh
-sed -e 's,dobin,mydobin,g;s,^libdir=.*$,if defined USRLOCAL\nthen\n\tbindir=/usr/local/bin\nelse\n\tlibdir=/usr/bin\nfi,' /usr/lib/cygport/bin/dobin > /usr/lib/cygport/bin/mydobin
+sed -e 's,dobin,mydobin,g;s,^bindir=.*$,if defined USRLOCAL\nthen\n\tbindir=/usr/local/bin\nelse\n\tlibdir=/usr/bin\nfi,' /usr/lib/cygport/bin/dobin > /usr/lib/cygport/bin/mydobin
 sed -e 's,dolib,mydolib,g;s,^libdir=.*$,if defined USRLOCAL\nthen\n\tlibdir=/usr/local/lib\nelse\n\tlibdir=/usr/lib\nfi,' /usr/lib/cygport/bin/dolib > /usr/lib/cygport/bin/mydolib
