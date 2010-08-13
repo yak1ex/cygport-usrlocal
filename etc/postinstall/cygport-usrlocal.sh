f4d800de955d0be29fdadc376cac90b45e580749
atarashi <atarashi@ca317041-1a13-0410-b3bf-f3d8fb9af60d>
2010-08-13 05:59:33 +0000

    Change mydobin and mydolib from scripts to shell functions.
    git-svn-id: https://yak.myhome.cx/repos/source/cygport-usrlocal/trunk@1068 ca317041-1a13-0410-b3bf-f3d8fb9af60d

--- etc/postinstall/cygport-usrlocal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sed -e 's,dobin,mydobin,g;s,^bindir=.*$,if defined USRLOCAL\nthen\n\tbindir=/usr/local/bin\nelse\n\tbindir=/usr/bin\nfi,' /usr/lib/cygport/bin/dobin > /usr/lib/cygport/bin/mydobin
-sed -e 's,dolib,mydolib,g;s,^libdir=.*$,if defined USRLOCAL\nthen\n\tlibdir=/usr/local/lib\nelse\n\tlibdir=/usr/lib\nfi,' /usr/lib/cygport/bin/dolib > /usr/lib/cygport/bin/mydolib
